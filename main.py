76e973d4b4e3d7c0034545ac67af89626ea576f9
chliste <86164003+mittal-notahaxer@users.noreply.github.com>
2021-08-03 13:48:59 +0530

    Updated main.py
    Added support for other "activities" and also reduced double usage of YOUR_TOKEN, now using token variable, defined once.

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 import discord, requests, json
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = "sc!")
+bot = commands.Bot(command_prefix = "!!") # I just like this one better
+token = "YOUR_TOKEN"
+
+app_ids = {
+    "chess": 832012774040141894,
+    "fish": 814288819477020702,
+    "poker": 755827207812677713,
+    "yt": 755600276941176913,
+    "betrayal": 773336526917861400
+    }
+
+def make(t, vcid):
+    t = app_ids[t.lower()]
+    url = f"https://discord.com/api/v9/channels/{vcid}/invites"
+    body = {
+        "max_age": 86400,
+        "max_uses": 0,
+        "target_application_id": f"{t}",
+        "target_type": 2,
+        "temporary": False,
+        "validate": None
+    }
+    auth = {
+        "Authorization": f"Bot {token}",
+        "Content-Type": "application/json",
+        "X-Ratelimit-Precision": "millisecond"
+    }
+
+    obj = json.dumps(body, separators=(',', ':'), ensure_ascii=True)
+    code = (requests.post(url, data = obj, headers = auth))
+    code = json.loads(code.text)["code"]
+
+    invite = f"https://discord.gg/{code}"
+    return invite
 
 @bot.event
 async def on_ready():
     print("Online")
 
 @bot.command()
-async def chess(ctx):
+async def play(ctx, game):
     try:
         voiceChannel = ctx.author.voice.channel
         if voiceChannel != None:
-            url = f"https://discord.com/api/v9/channels/{voiceChannel.id}/invites"
-            body = {
-                "max_age": 1800,
-                "max_uses": 0,
-                "target_application_id": "832012774040141894",
-                "target_type": 2,
-                "temporary": False,
-                "validate": None
-            }
-            auth = {
-                "Authorization": "Bot TOKEN_HERE",
-                "Content-Type": "application/json",
-                "X-Ratelimit-Precision": "millisecond"
-            }
-
-            obj = json.dumps(body, separators=(',', ':'), ensure_ascii=True)
-            code = (requests.post(url, data = obj, headers = auth))
-            code = json.loads(code.text)["code"]
-            invite = f"https://discord.gg/{code}"
-            await ctx.send(invite)
+            await ctx.send(make(game, voiceChannel.id))
         else:
             await ctx.send("Connect to VC first")
     except:
         await ctx.send("Connect to VC first")
-
-bot.run("TOKEN_HERE")
+    
+bot.run(token)
