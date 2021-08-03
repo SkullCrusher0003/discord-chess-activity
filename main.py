ea6843b3078b0624f8dd0fef8c39711f1c1232cd
Skull Crusher <77442248+SkullCrusher0003@users.noreply.github.com>
2021-08-03 10:16:19 +0530

    Updated imports

--- main.py
+++ main.py
@@ -5,37 +5,36 @@ bot = commands.Bot(command_prefix = "sc!")
 
 @bot.event
 async def on_ready():
-  print("Online")
-  
+    print("Online")
+
 @bot.command()
 async def chess(ctx):
-  try:
-    voiceChannel = ctx.author.voice.channel
-    if voiceChannel != None:
-      url = f"https://discord.com/api/v9/channels/{voiceChannel.id}/invites"
-      body = {
-        "max_age": 1800,
-        "max_uses": 0,
-        "target_application_id": "832012774040141894",
-        "target_type": 2,
-        "temporary": False,
-        "validate": None
-       }
-      auth = {
-        "Authorization": "Bot TOKEN_HERE",
-        "Content-Type": "application/json",
-        "X-Ratelimit-Precision": "millisecond"
-       }
+    try:
+        voiceChannel = ctx.author.voice.channel
+        if voiceChannel != None:
+            url = f"https://discord.com/api/v9/channels/{voiceChannel.id}/invites"
+            body = {
+                "max_age": 1800,
+                "max_uses": 0,
+                "target_application_id": "832012774040141894",
+                "target_type": 2,
+                "temporary": False,
+                "validate": None
+            }
+            auth = {
+                "Authorization": "Bot TOKEN_HERE",
+                "Content-Type": "application/json",
+                "X-Ratelimit-Precision": "millisecond"
+            }
 
-      obj = json.dumps(body, separators=(',', ':'), ensure_ascii=True)
-      code = (requests.post(url, data = obj, headers = auth))
-      code = json.loads(code.text)["code"]
+            obj = json.dumps(body, separators=(',', ':'), ensure_ascii=True)
+            code = (requests.post(url, data = obj, headers = auth))
+            code = json.loads(code.text)["code"]
+            invite = f"https://discord.gg/{code}"
+            await ctx.send(invite)
+        else:
+            await ctx.send("Connect to VC first")
+    except:
+        await ctx.send("Connect to VC first")
 
-      invite = f"https://discord.gg/{code}"
-      await ctx.send(invite)
-    else:
-      await ctx.send("Connect to VC first")
-  except:
-    await ctx.send("Connect to VC first")
-    
 bot.run("TOKEN_HERE")
